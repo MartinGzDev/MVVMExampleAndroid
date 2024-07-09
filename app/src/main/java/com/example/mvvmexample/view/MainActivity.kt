755d5d1c760982d63f1e3808d66b4ce22af6d1af
Developer Mobile Martin <martingz@openix.com.ar>
2024-07-09 17:39:19 -0300

    add MVVM

---
+++ app/src/main/java/com/example/mvvmexample/view/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.mvvmexample.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.mvvmexample.R
+import com.example.mvvmexample.databinding.ActivityMainBinding
+import com.example.mvvmexample.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        */
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+        })
+
+        binding.mainContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
+    }
+}
